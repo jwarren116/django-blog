61701a784b2b7ab0e22a951547587096132822e7
James Warren <jwarren116@gmail.com>
2015-04-03 15:39:33 -0700

    Add success message when contact form is completed

--- contact/views.py
+++ contact/views.py
@@ -3,9 +3,12 @@ from contact.forms import ContactForm
 
 
 def contact(request):
+    success = False
     if request.method == 'POST':
         email_info = ContactForm(request.POST)
         if email_info.is_valid():
             email_info.save()
+            success = True
     return render(request, 'contact.html', {
+        'success': success,
     })
