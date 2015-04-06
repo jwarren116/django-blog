bfd16f83f31f4f62a278d72615056b1ca14f1c83
James Warren <jwarren116@gmail.com>
2015-04-05 21:08:39 -0700

    Remove contact route from urls

--- Sanctuary/urls.py
+++ Sanctuary/urls.py
@@ -6,5 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('blog.urls')),
-    url(r'^contact/', 'contact.views.contact', name='contact'),
+    # url(r'^contact/', 'contact.views.contact', name='contact'),
 )
