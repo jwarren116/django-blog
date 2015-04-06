a76b6aadf70102c0abf19b3b008c68873296dbf7
James Warren <jwarren116@gmail.com>
2015-04-05 21:09:42 -0700

    Begin rough build out of Project model to eventually eliminate hardcoding of projects on index page

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,21 @@ class BlogPost(models.Model):
     class Meta:
         verbose_name_plural = "Blog Posts"
 
-    title = models.CharField('Title', max_length=120)
+    title = models.CharField('Title', max_length=63)
     post = models.TextField('Content')
     created = models.DateTimeField('Created', auto_now_add=True, auto_now=False)
     display = models.BooleanField('Display on Home Page?', default=False)
     heading = models.BooleanField('Display as Heading?', default=False)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=63)
+    screen_shot = models.ImageField(upload_to='screen_shots')
+    description = models.TextField()
+    link = models.URLField()
 
     def __unicode__(self):
         return self.title
