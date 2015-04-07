ac6976d76ae6d0d1076214f948c6814d7dbaa0fa
James Warren <jwarren116@gmail.com>
2015-04-07 15:10:57 -0700

    Reduce opacity of background to 20% from 30%

--- blog/static/blog/js/blog.js
+++ blog/static/blog/js/blog.js
@@ -1,5 +1,5 @@
 $.backstretch("/static/blog/img/subway-1600.jpg");
-$('.backstretch').css("opacity", 0.3);
+$('.backstretch').css("opacity", 0.2);
 
 function autoScrollTo(elem) {
     var top = $("#" + elem).offset().top - 150;
