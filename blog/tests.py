ffc4ee56d265b8371af6138c828f6733769b66d3
James Warren <jwarren116@gmail.com>
2015-04-23 12:13:28 -0700

    Add Post Factory to tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,22 @@
 from django.test import LiveServerTestCase
+from blog.models import BlogPost
 from selenium import webdriver
+import factory
+
+
+class PostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = BlogPost
+
+    title = 'Post'
+    post = 'This is a new post'
+    display = True
 
 
 class TestProfileView(LiveServerTestCase):
     def setUp(self):
         self.selenium = webdriver.Firefox()
+        self.post = PostFactory.create()
         super(TestProfileView, self).setUp()
 
     def tearDown(self):
@@ -13,18 +25,15 @@ class TestProfileView(LiveServerTestCase):
 
     def test_home(self):
         '''test that home page is available to visitor'''
-        # self.selenium.get(self.live_server_url)
+        self.selenium.get(self.live_server_url)
         assert self.selenium.find_element_by_id('projects')
         assert self.selenium.find_element_by_id('about')
         assert self.selenium.find_element_by_id('posts')
 
     def test_posts(self):
         '''test that post details and return links are available to visitor'''
-        # self.selenium.get(self.live_server_url)
+        self.selenium.get(self.live_server_url)
         self.selenium.find_element_by_partial_link_text('Read more').click()
         assert self.selenium.find_element_by_class_name('detail-entry')
         self.selenium.find_element_by_partial_link_text('James Warren').click()
         assert self.selenium.find_element_by_id('summary')
-
-    def test_link_scrolling(self):
-        pass
