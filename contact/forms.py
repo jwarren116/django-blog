8e04e7d8b76ff720aa69c8269eb32e6499841411
James Warren <jwarren116@gmail.com>
2015-04-23 12:08:52 -0700

    Remove old contact app

--- contact/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from contact.models import ContactMe
-
-
-class ContactForm(ModelForm):
-    class Meta:
-        model = ContactMe
-        fields = ['email', 'subject', 'body']
